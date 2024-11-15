616ddbda80b355ae632ea8828cd83d95550b6fdc
yakutdmitry <159444433+yakutdmitry@users.noreply.github.com>
2024-11-15 14:56:51 +0000

    moving objects sctipt ready

--- Assets/Scripts/MovingObjects.cs
+++ Assets/Scripts/MovingObjects.cs
@@ -1,17 +1,33 @@
+using System;
 using UnityEngine;
 
 public class MovingObjects : MonoBehaviour
 {
     [SerializeField] private float rayDistance = 10f;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform holdPoint;
+    private GameObject heldObject;
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("BUTTON PRESSED");
+            if (heldObject == null)
+            {
+                Debug.Log("its null");
+                MoveObject();
+            }
+            else
+            {
+                Debug.Log("else");
+                DropObject();
+            }
+        }
+        
+    }
+    
+    private void MoveObject()
     {
         Camera camera = Camera.main;
 
@@ -25,11 +41,21 @@ public class MovingObjects : MonoBehaviour
         {
             if (hit.collider.CompareTag("Movable"))
             {
-                Debug.Log("Box Detected");
+                Debug.Log("Detected");
+                heldObject = hit.collider.gameObject;
+                heldObject.transform.SetParent(holdPoint);
+                heldObject.transform.localPosition = Vector3.zero;
+                heldObject.GetComponent<Rigidbody>().isKinematic = true;
             }
-
         }
-
         Debug.DrawRay(RayOrigin, rayDirection * rayDistance, Color.red);
+        
+    }
+
+    private void DropObject()
+    {
+        heldObject.transform.SetParent(null);
+        heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        heldObject = null;
     }
 }
